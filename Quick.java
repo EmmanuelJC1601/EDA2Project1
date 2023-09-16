6cd93f4dacb63e523ac306d58be5f6187bea78ad
Karla <kp.ruizcervantes@gmail.com>
2023-09-15 23:55:40 -0600

    Quick sort actualizado

--- Quick.java
+++ Quick.java
@@ -1,32 +1,57 @@
 public class Quick{
 
-    private static long operationCount = 0;
-
-    public static int partition(int arr[], int low, int high){
+    /*public static int partition(int arr[], int low, int high, long count){
     int pivot = arr[high];
+    count ++;
+
     int i = (low-1);
     for (int j=low; j<high; j++){
-        if (arr[j] <= pivot)
-        {
+        if (arr[j] <= pivot){
             i++;
             Utilerias.swap(arr, i,j);
-            operationCount++;
+            count++;
 
         }
     }
         int temp = arr[i+1];
         arr[i+1] = arr[high];
         arr[high] = temp;
-        operationCount++;
+        count += 3;
         return i+1;
-    }
+    }*/
 
-    public static long QuickSort(int arr[], int low, int high){
+    //public static long count = 0; 
+
+    public static long QuickSort(int arr[], int low, int high, long count){
+
+        //long count = 0;
+
         if (low < high){
-            int pi = partition(arr, low, high);
-            QuickSort(arr, low, pi-1);
-            QuickSort(arr, pi+1, high);
+
+            int pivot = arr[high];
+            count ++;
+
+            int i = (low-1);
+            for (int j=low; j<high; j++){
+                if (arr[j] <= pivot){
+                    i++;
+                    Utilerias.swap(arr, i,j);
+                    count++;
+                }
+            }
+
+            int temp = arr[i+1];
+            arr[i+1] = arr[high];
+            arr[high] = temp;
+            count += 3;
+
+            int pi = i + 1;
+
+            long countLeft = QuickSort(arr, low, pi-1, count);
+            long countRight = QuickSort(arr, pi+1, high, count);
+
+            count = countLeft + countRight;
         }
-        return operationCount;
+        return count;
     }
 }
